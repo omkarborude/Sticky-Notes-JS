a8df36346cdeb1d19e6c96698294a7db13928941
omkar <borudeomkar41@gmail.com>
2021-01-16 15:54:52 +0530

    Refactore

--- back.js
+++ back.js
@@ -44,6 +44,8 @@ checkBTN.addEventListener("click", () => {
 
     sticktext.innerHTML = getin;
 
+    stick.style.background = stickcolor();
+
     stick.appendChild(sticktext);
 
     cont2.insertAdjacentElement("beforeend",stick);
@@ -51,3 +53,13 @@ checkBTN.addEventListener("click", () => {
     
 })
 
+function stickcolor() {
+    var any_color = ["#3EADCF","#F1A7F1","#FFD8CB","#BBDBBE","#03C8A8","#F3E6E8"];
+      
+    if (i > any_color.length - 1) {
+        i = 0 ;
+    }
+
+    return any_color[i++];
+
+}
