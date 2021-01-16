3e7480100f9e6213cb3e62bd88c56a2c41a517b7
omkar <borudeomkar41@gmail.com>
2021-01-16 12:54:01 +0530

    Update

---
+++ back.js
@@ -0,0 +1,8 @@
+var cont2 = document.getElementsByClassName(".cont2");
+var cont3 = document.getElementsByClassName(".cont3");
+
+var createBTN = document.getElementById('#BTN');
+var checkBTN = document.getElementById('#checkBTN');
+var cutBTN = document.getElementById('#cutBTN');
+
+var i = 0;
