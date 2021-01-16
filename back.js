e74ef309a84b4a646a30e1df008fce69ca8ba109
omkar <borudeomkar41@gmail.com>
2021-01-16 13:05:59 +0530

    Refactore

--- back.js
+++ back.js
@@ -1,8 +1,14 @@
-var cont2 = document.getElementsByClassName(".cont2");
-var cont3 = document.getElementsByClassName(".cont3");
+var cont2 = document.getElementsByClassName("cont2")[0];
+var cont3 = document.getElementsByClassName("cont3")[0];
 
 var createBTN = document.getElementById('#BTN');
 var checkBTN = document.getElementById('#checkBTN');
 var cutBTN = document.getElementById('#cutBTN');
 
 var i = 0;
+
+createBTN.addEventListener("click", function(){
+    if(cont3.style.display == "none"){
+        cont3.style.display = "block";
+    }
+});
