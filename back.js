baf6876b142c9f4120271f1294f516a4c3e49b97
omkar <borudeomkar41@gmail.com>
2021-01-16 13:17:09 +0530

    update

--- back.js
+++ back.js
@@ -1,14 +1,24 @@
 var cont2 = document.getElementsByClassName("cont2")[0];
 var cont3 = document.getElementsByClassName("cont3")[0];
 
-var createBTN = document.getElementById('#BTN');
-var checkBTN = document.getElementById('#checkBTN');
-var cutBTN = document.getElementById('#cutBTN');
+var createBTN = document.getElementById('BTN');
+var checkBTN = document.getElementById('checkBTN');
+var cutBTN = document.getElementById('cutBTN');
 
 var i = 0;
 
-createBTN.addEventListener("click", function(){
+// opening notepad
+createBTN.addEventListener("click", () => {
     if(cont3.style.display == "none"){
         cont3.style.display = "block";
-    }
+      }
+      else{
+        cont3.style.display = "block";
+      }
 });
+// cloasing notepad
+cutBTN.addEventListener("click", () => {
+   if
+} )
+
+
