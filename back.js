8b401e5c15fdbed6ab39db1721011689aa33fc9c
omkar <borudeomkar41@gmail.com>
2021-01-16 17:31:56 +0530

    Final

--- back.js
+++ back.js
@@ -39,20 +39,37 @@ checkBTN.addEventListener("click", () => {
     var stick = document.createElement("div");
     stick.id = 'stick';
 
+    var trashBTN = document.createElement('button');
+    trashBTN.innerHTML = '<i class="fas fa-border-none fa-trash-alt"></i>';
+    trashBTN.id = 'trashBTN';
+
     var sticktext = document.createElement('h1');
     sticktext.id = 'sticktext';
 
+    
+
     sticktext.innerHTML = getin;
 
+    
     stick.style.background = stickcolor();
+    trashBTN.style.background = stick.style.background;
 
+    stick.appendChild(trashBTN);
     stick.appendChild(sticktext);
 
     cont2.insertAdjacentElement("beforeend",stick);
+    
+    document.getElementById('getin').value = '';
 
+    // delete BTN
+    trashBTN.addEventListener("click", () => {
+        stick.remove();
+    })
     
 })
 
+
+// change bg color randamly
 function stickcolor() {
     var any_color = ["#3EADCF","#F1A7F1","#FFD8CB","#BBDBBE","#03C8A8","#F3E6E8"];
       
